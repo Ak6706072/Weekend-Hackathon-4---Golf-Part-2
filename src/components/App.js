9dfbc06429645b019c7f936e1d2cfd9a9bca2802
Ashish kumar <ak6706072@gmail.com>
2021-02-07 00:44:51 +0530

    change in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
+
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
@@ -9,9 +10,97 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const fun = (event)=>{
+      console.log(event.keyCode);
+      switch(event.keyCode){
+        case 39:{
+          //right arrow key
+          setX(x+5);
+          setBallPosition({
+            left:x + "px",
+            top:y + "px",
+            position : "absolute"
+          })
+          // console.log(ballPosition,x,y)
+          break;
+        }
+        case 40:{
+            //down arrow key
+            setY(y+5);
+            // console.log("down arrow key",x,y)
+            setBallPosition({
+              left:x + "px",
+              top:y + "px",
+              position : "absolute"
+            })
+            // console.log(ballPosition,x,y)
+            break;
+        }
+        case 38:{
+          //up arrow key
+          
+            setY(y-5);
+            // console.log("down arrow key",x,y)
+            setBallPosition({
+              left:x + "px",
+              top:y + "px",
+              position : "absolute"
+            })
+            // console.log(ballPosition,x,y)
+          break;
+        }
+        case 37:{
+          // left arrow key
+         
+            setX(x-5);
+          setBallPosition({
+            left:x + "px",
+            top:y + "px",
+            position : "absolute"
+          })
+          // console.log(ballPosition,x,y)
+          break;
+        }
+        default :{
+          break;
+        }
+      }
+    }
+  
+  useEffect(()=>{
+    console.log("useEffect body");
+      document.addEventListener("keydown",fun)
+      //cleanUp function
+      return () =>{
+        document.removeEventListener("keydown",fun);
+        // console.log("cleanup function")
+      }
+  },[ballPosition])
+  // every x,y changes then again executed the useeffect body and 
+  // every useeffect attact the event an the document the page is very heavy 
+  //in every x,y change  so we need to write the clean up documet event listener when 
+  // we change the x,y
+  const reset = () => {
+      setRenderBall(false);
+      setX(0);
+      setY(0);
+      setBallPosition({
+        left: "0px",
+        top:"0px"
+      })
+  };
+  const clickHandler = ()=>{
+    setRenderBall(true);
+  }
+  const renderChoice = () => {
+    if(!renderBall){
+      return <button onClick={clickHandler} className="start">Start</button>
+    }else{
+      return <div className="ball" style={{...ballPosition,position:"absolute"}}></div>
+    }
 
+  };
+  // console.log(renderChoice())
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
