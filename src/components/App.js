04b42ec944c6e701edd7c2e5f9b2c7eb7a1bc835
Ashish kumar <ak6706072@gmail.com>
2021-02-09 05:54:45 +0530

    resolve

--- src/components/App.js
+++ src/components/App.js
@@ -6,34 +6,15 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-<<<<<<< HEAD
   const [ballPosition,setBallPosition] = useState({ left: "0px", top: "0px",});
   
- 
  
  useEffect(()=>{
         setBallPosition((ballPosition)=>{
             return {...ballPosition,left:x+"px",top:y+"px"}
-=======
-  const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-  });
-  const fun = (event)=>{
-      //console.log(event.keyCode);
-      switch(event.keyCode){
-        case 39:{
-          //right arrow key
-          setX(x+5);
-          setBallPosition({
-            left:x + "px",
-            top:y + "px",
-            position : "absolute"
->>>>>>> 7195d2bbf9db544c4d553e9e25e900c27b82e884
-          })
+           });
  },[x,y])
-
-
+ 
  let handleClick = (event) => {
       switch (event.keyCode) {
         case 39:
@@ -73,20 +54,10 @@ const App = () => {
     return ()=> {
       document.removeEventListener("keydown",handleClick);
     }
-<<<<<<< HEAD
+
  },[ballPosition])
-=======
+
   
-  useEffect(()=>{
-    //console.log("useEffect body");
-      document.addEventListener("keydown",fun)
-      //cleanUp function
-      return () =>{
-        document.removeEventListener("keydown",fun);
-        // console.log("cleanup function")
-      }
-  },[ballPosition])
->>>>>>> 7195d2bbf9db544c4d553e9e25e900c27b82e884
   // every x,y changes then again executed the useeffect body and 
   // every useeffect attact the event an the document the page is very heavy 
   //in every x,y change  so we need to write the clean up documet event listener when 
